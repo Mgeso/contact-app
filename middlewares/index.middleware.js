fc597a05a81935813a5f9f40ae429312129aacb2
Mgeso <106961023+Mgeso@users.noreply.github.com>
2022-09-23 10:11:04 +0100

    update

--- middlewares/index.middleware.js
+++ middlewares/index.middleware.js
@@ -14,6 +14,12 @@ const middleware = (app) => {
   app.use(cors());
   app.use(express.urlencoded({ extended: false }));
 
+  app.use('*', (req, res) => {
+    res.status(200).send({
+      success: true, message: 'server is up and running'
+    });
+  });
+
   database();
   app.use(router);
 
